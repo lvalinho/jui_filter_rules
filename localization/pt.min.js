9791f63d123148b91f181ab259ad42bd070ab0ca
Luciano <luciano.valinho@gmail.com>
2016-10-31 14:34:27 +0000

    Create pt.min.js

---
+++ localization/pt.min.js
@@ -0,0 +1,56 @@
+/**
+ * jui_filter_rules v1.0.7 simple localization - EUROPEAN PORTUGUESE
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ * 
+ * by Luciano Valinho
+ */
+var rsc_jui_fr = {
+
+    no_filters_found: "Não há filtros definidos",
+
+    rules_group_AND: "todas as regras",
+    rules_group_OR: "qualquer regra",
+
+    rule: "regra",
+    group: "grupo",
+    tools_please_select: "&raquo;",
+    rule_insert_before: "inserir antes",
+    rule_insert_after: "inserir depois",
+    rule_insert_inside: "inserir dentro",
+    rule_clear: "limpar",
+    rule_delete: "eliminar",
+    group_insert_before: "inserir antes",
+    group_insert_after: "inserir depois",
+    group_insert_inside: "inserir dentro",
+    group_delete: "eliminar",
+
+    filter_please_select: '&raquo; seleccionar',
+
+    operator_equal: 'igual',
+    operator_not_equal: 'diferente',
+    operator_in: 'in',
+    operator_not_in: 'not in',
+    operator_less: 'menor',
+    operator_less_or_equal: 'menor ou igual',
+    operator_greater: 'maior',
+    operator_greater_or_equal: 'maior ou igual',
+    operator_between: 'entre',
+    operator_not_between: 'não esteja entre',
+    operator_begins_with: 'começa com',
+    operator_not_begins_with: 'não começa com',
+    operator_contains: 'contém',
+    operator_not_contains: 'não contém',
+    operator_ends_with: 'termina com',
+    operator_not_ends_with: 'não termina com',
+    operator_is_empty: 'está vazio',
+    operator_is_not_empty: 'não está vazio',
+    operator_is_null: 'é nulo',
+    operator_is_not_null: 'não é nulo',
+
+    error_no_value_given: "Valor não foi dado",
+    error_invalid_number: "o valor não é numérico",
+    error_invalid_datetime: "formato inválido",
+    error_converting_value: "Erro ao converter valor"
+};
